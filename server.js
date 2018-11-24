a9c685313127d002a79677c11cb8cc8f3ebd02a1
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-24 14:50:30 -0500

    added options to connection to freeze tablenames and to user model to have no timestamps

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ const port = 8080;
 
 
 // config options for connecting to the database
+// define property sets options for all models
+// freezeTableName true does not pluralize the table name
 const connection = new Sequelize({
-  "username": 'root',
-  "password": 'root',
-  "database": 'testDB',
-  "host": 'localhost',
-  "dialect": 'mysql'
+  username: 'root',
+  password: 'root',
+  database: 'testDB',
+  host: 'localhost',
+  dialect: 'mysql',
+  define: {
+    freezeTableName: true,
+  }
 });
 
 // define the model for the user table
 // uuid is a universal unique id, setting default value keeps it from being null
+// the 3rd argument sets properties on the modle
+// timestamps false set no created or updated fields are created
 const User = connection.define('User', {
   uuid: {
     type: Sequelize.UUID,
@@ -23,8 +30,12 @@ const User = connection.define('User', {
     defaultValue: Sequelize.UUIDV4
   },
   name: Sequelize.STRING,
-  bio: Sequelize.TEXT
-});
+  bio: Sequelize.TEXT,
+}, 
+{
+  timestamps: false
+}
+);
 
 // connect and sync the database then log success
 // logging option shows the sql in the console
