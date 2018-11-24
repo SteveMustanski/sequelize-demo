3366f188000d0a3296c1156874e6d8dc7ce033b4
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-24 13:32:05 -0500

    added a UUID to the user model

--- server.js
+++ server.js
@@ -15,16 +15,30 @@ const connection = new Sequelize({
 });
 
 // define the model for the user table
+// uuid is a universal unique id, setting default value keeps it from being null
 const User = connection.define('User', {
+  uuid: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4
+  },
   name: Sequelize.STRING,
   bio: Sequelize.TEXT
 });
 
 // connect and sync the database then log success
 // logging option shows the sql in the console
+// setting force to true will drop the tables and recreate on connection
 connection
 .sync({
-  logging: console.log
+  logging: console.log,
+  force: true
+})
+.then(() => {
+  User.create({
+    name: 'Sally',
+    bio: 'New bio entry for Sally'
+  })
 })
 .then(() => {
   console.log('Connection to db successfull');
