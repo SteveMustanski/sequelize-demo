10b70b036dbaa64062361de4aea247c6ab7faa75
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-24 13:08:32 -0500

    updates to server.js to connect to local mysql database

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const Sequelize = require('sequelize');
 const app = express();
 const port = 8080;
 
+const connection = new Sequelize({
+  "username": 'root',
+  "password": 'root',
+  "host": 'localhost',
+  "dialect": 'mysql'
+});
+
+connection
+.authenticate()
+.then(() => {
+  console.log('Connection to db successfull');
+})
+
 
 app.listen(port, () => {
   console.log(`Running server on port ${port}!`);
