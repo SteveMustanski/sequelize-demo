6033fa734b754aac5a518df2e6751268be5fb3a1
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-24 13:20:14 -0500

    added user model to server.js

--- server.js
+++ server.js
@@ -4,20 +4,33 @@ const Sequelize = require('sequelize');
 const app = express();
 const port = 8080;
 
+
+// config options for connecting to the database
 const connection = new Sequelize({
   "username": 'root',
   "password": 'root',
+  "database": 'testDB',
   "host": 'localhost',
   "dialect": 'mysql'
 });
 
+// define the model for the user table
+const User = connection.define('User', {
+  name: Sequelize.STRING,
+  bio: Sequelize.TEXT
+});
+
+// connect and sync the database then log success
+// logging option shows the sql in the console
 connection
-.authenticate()
+.sync({
+  logging: console.log
+})
 .then(() => {
   console.log('Connection to db successfull');
-})
+});
 
-
+// start listening on port
 app.listen(port, () => {
   console.log(`Running server on port ${port}!`);
 })
